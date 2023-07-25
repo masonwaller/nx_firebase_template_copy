7d37f361dbc68885c3e9ae2635c8206cc15abfed
Mason Waller <masonwaller805@gmail.com>
2023-07-25 09:44:36 -0600

    testing parrallel

--- apps/frontend-e2e/cypress.config.ts
+++ apps/frontend-e2e/cypress.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 export default defineConfig({
+  projectId: "eeaxwz",
   e2e: nxE2EPreset(__dirname),
 });
