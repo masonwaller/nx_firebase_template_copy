965c29aef22f71184b9023f8bd754242094b6000
Mason Waller <masonwaller805@gmail.com>
2023-08-02 13:32:53 -0700

    finished testing

--- apps/frontend-e2e/cypress.config.ts
+++ apps/frontend-e2e/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 export default defineConfig({
-  projectId: "eeaxwz",
+  // projectId: "eeaxwz",
   e2e: {...nxE2EPreset(__dirname),
     setupNodeEvents(on,config) { 
       on('task', {
